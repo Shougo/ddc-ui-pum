f121e44c96176229aa8ccc09da365eacda96e035
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-11-26 13:56:35 +0900

    Update

--- denops/@ddc-uis/pum.ts
+++ denops/@ddc-uis/pum.ts
@@ -1,10 +1,26 @@
-import { Context, DdcItem } from "https://deno.land/x/ddc_vim@v3.2.0/types.ts";
-import { BaseUi } from "https://deno.land/x/ddc_vim@v3.2.0/base/ui.ts";
-import { Denops } from "https://deno.land/x/ddc_vim@v3.2.0/deps.ts";
+import { Context, DdcItem } from "https://deno.land/x/ddc_vim@v3.3.0/types.ts";
+import { BaseUi } from "https://deno.land/x/ddc_vim@v3.3.0/base/ui.ts";
+import { autocmd, Denops } from "https://deno.land/x/ddc_vim@v3.3.0/deps.ts";
 
 export type Params = Record<never, never>;
 
 export class Ui extends BaseUi<Params> {
+  override async onInit(args: {
+    denops: Denops;
+  }) {
+    await autocmd.group(
+      args.denops,
+      "ddc-ui-pum",
+      (helper: autocmd.GroupHelper) => {
+        helper.define(
+          "User",
+          "PumCompleteDone",
+          "ddc#on_complete_done(g:pum#completed_item)",
+        );
+      },
+    );
+  }
+
   override async skipCompletion(args: {
     denops: Denops;
   }): Promise<boolean> {
