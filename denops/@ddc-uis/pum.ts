7e568274b3c22ca8d1742ddec43ea3a63d6ab7e9
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-09-03 13:38:29 +0900

    Update deps

--- denops/@ddc-uis/pum.ts
+++ denops/@ddc-uis/pum.ts
@@ -1,10 +1,10 @@
-import { type Context, type DdcItem } from "jsr:@shougo/ddc-vim@6.0.0/types";
-import { BaseUi } from "jsr:@shougo/ddc-vim@6.0.0/ui";
+import { type Context, type DdcItem } from "jsr:@shougo/ddc-vim@~7.0.0/types";
+import { BaseUi } from "jsr:@shougo/ddc-vim@~7.0.0/ui";
 
-import type { Denops } from "jsr:@denops/core@^7.0.0";
+import type { Denops } from "jsr:@denops/core@~7.0.0";
 
-import * as fn from "jsr:@denops/std@7.0.1/function";
-import * as autocmd from "jsr:@denops/std@7.0.1/autocmd";
+import * as fn from "jsr:@denops/std@~7.1.1/function";
+import * as autocmd from "jsr:@denops/std@~7.1.1/autocmd";
 
 export type Params = {
   insert: boolean;
